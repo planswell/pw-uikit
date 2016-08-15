53114ffd7715d13ba5760f2f73dcb9a10407add8
Ken Ip <ken@planswell.ca>
2016-08-15 17:04:56 -0400

    Initial implementation of packages.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Planswell UIKit
+
+Planswell UIKit is a collection of React UI components heavily inspired by CloudFlare's React UIKit. It is constructed
+using [Lerna](https://github.com/lerna/lerna) to easily package many npm packages under packages for simplified
+development and testing. The key differences with CloudFlare's implementation are:
+
+ * Full ES6 everywhere
+ * CSS Modules support
+ * Test via mocha & enzyme (same as pw-react)
+
+## Development
+
+The development environment is [React Storybook](https://github.com/kadirahq/react-storybook). Run `npm start` to
+launch React Storybook and you'll see composed stories there. Add a new story to an existing component for new
+behaviours or add new story files for new components. See [React Storybook](https://github.com/kadirahq/react-storybook)
+for more.
+
+## Testing
+
+Testing is done within each component by leveraging the webpack configuration with `internals/`. The entire testing
+setup mirrors our pw-react project. Simply run the same scripts to test (such as `npm test` and `npm run test:watch`).
+
+## Future Enhancement
+
+ * PostCSS Integration
---
+++ packages/pw-component-button/index.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react';
+import cssModules from 'react-css-modules';
+import styles from './style.css';
+
+class Button extends React.Component {
+  static propTypes = {
+    children: PropTypes.array,
+    onClick: PropTypes.func.isRequired,
+    submit: PropTypes.bool,
+    type: PropTypes.oneOf([
+      'default',
+      'primary',
+      'success',
+      'warning',
+      'danger',
+    ]).isRequired,
+    disabled: PropTypes.bool,
+    loading: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    type: 'default',
+    submit: false,
+  };
+
+  render() {
+    const { disabled, loading } = this.props;
+    const buttonType = this.props.submit ? 'submit' : 'button';
+
+    // Unless the disabled state is explicitly set, the button is disabled when loading.
+    const isDisabled = (disabled !== undefined ? disabled : loading) || false;
+
+    return (
+      <button
+        type={buttonType}
+        styleName={this.props.type}
+        disabled={isDisabled}
+        onClick={this.props.onClick}
+      >
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+export default cssModules(Button, styles);
