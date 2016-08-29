dc6c53495b0d9499c222cd48a5c01d84fae4d0ba
Ken Ip <kenips@users.noreply.github.com>
2016-08-29 13:23:54 -0400

    Fix PostCSS plugin orders causing FAwesome to break. (#20)
    Fixes #18.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -46,13 +46,13 @@ module.exports = {
   postcss: () => ([
     postcssImport(),
     postcssFocus(), // Add a :focus to every :hover
-    cssnext({ // Allow future CSS features to be used, also auto-prefixes the CSS...
-      browsers: ['last 2 versions', 'IE > 10'], // ...based on this browser list
-    }),
     postcssReporter({ // Posts messages from plugins to the terminal
       clearMessages: true,
     }),
-    postcssFontMagician({ custom: fontAwesomeConfig }),
     postcssFontAwesome,
+    postcssFontMagician({ custom: fontAwesomeConfig }), // Must be after FontAwesome
+    cssnext({ // Should be the end since this add vendor prefixes
+      browsers: ['last 2 versions', 'IE > 10'], // ...based on this browser list
+    }),
   ]),
 };
