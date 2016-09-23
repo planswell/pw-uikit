2599dbfb4a0817706101758f32ed51bd8b5509d4
Quinn Strahl <qstrahl@gmail.com>
2016-09-23 14:50:22 -0400

    Add activeClassName to link component & fix button style

--- packages/pw-component-link/index.js
+++ packages/pw-component-link/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 import cssModules from 'react-css-modules';
 import cssStyles from './style.css';
-import omit from 'lodash/omit';
 
 const { PropTypes } = React;
 
-const OurLink = ({ button, children, ...others }) => {
-  const props = omit(others, 'styles');
+const OurLink = ({ button, children, styles, ...props }) => {
   const styleName = button ? 'button' : 'link';
 
   return (
-    <Link styleName={`base ${styleName}`} {...props}>
+    <Link styleName={`base ${styleName}`} activeClassName={styles.active} {...props}>
       <span styleName="inner">{children}</span>
     </Link>
   );
@@ -25,6 +23,7 @@ OurLink.propTypes = {
   ]),
   disabled: PropTypes.bool,
   href: PropTypes.string,
+  styles: PropTypes.object,
 };
 
 const cssModuleOptions = {
