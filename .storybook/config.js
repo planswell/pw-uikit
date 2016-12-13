f353afdc6ddc2f24b4afa62e99cdf9b5601770f3
Ken Ip <ken@planswell.ca>
2016-12-13 17:23:15 -0500

    Update fontello to 20161213

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { configure } from '@kadira/storybook';
 import 'sanitize.css/sanitize.css';
+import '../packages/pw-base-css/custom-font-faces.css';
 
 function loadStories() {
   require('./../stories');
--- stories/icon.js
+++ stories/icon.js
@@ -3,12 +3,21 @@ import { storiesOf } from '@kadira/storybook';
 import Icon from './../packages/pw-component-icon';
 
 storiesOf('Icon', module)
-  .add('check', () => (
-    <Icon name="check" />))
-  .add('house', () => (
-    <Icon name="error" />))
-  .add('house', () => (
-    <Icon name="house" />))
-  .add('umbrella', () => (
-    <Icon name="umbrella" />))
+  .add('envelope', () => <Icon name="envelope" />)
+  .add('error', () => <Icon name="error" />)
+  .add('credit-card', () => <Icon name="credit-card" />)
+  .add('handset', () => <Icon name="handset" />)
+  .add('hand-and-coin', () => <Icon name="hand-and-coin" />)
+  .add('bullet-point', () => <Icon name="bullet-point" />)
+  .add('finance-details', () => <Icon name="finance-details" />)
+  .add('coin-up-arrow', () => <Icon name="coin-up-arrow" />)
+  .add('house', () => <Icon name="house" />)
+  .add('left-arrow-circle', () => <Icon name="left-arrow-circle" />)
+  .add('mortarboard', () => <Icon name="mortarboard" />)
+  .add('smile', () => <Icon name="smile" />)
+  .add('check', () => <Icon name="check" />)
+  .add('umbrella', () => <Icon name="umbrella" />)
+  .add('arrow', () => <Icon name="arrow" />)
+  .add('check-in-circle', () => <Icon name="check-in-circle" />)
+  .add('cross', () => <Icon name="cross" />)
   ;
