1b114d6f0422b1e24f538e80afcdd6013d688e36
Izzie Colpitts-Campbell <izzieccampbell@gmail.com>
2016-12-16 16:44:48 -0500

    fix checkbox incrementor

--- packages/pw-component-checkbox/tests/index.js
+++ packages/pw-component-checkbox/tests/index.js
@@ -7,9 +7,16 @@ import React from 'react';
 describe('<Checkbox />', () => {
   it('renders', () => {
     const renderedComponent = shallow(
-      <Checkbox label="yes" />
+      <Checkbox label="no" />
     );
 
     expect(renderedComponent.containsMatchingElement(<input type="checkbox" />)).toEqual(true);
+  });
+  it('renders when default checked', () => {
+    const rendered = shallow(
+      <Checkbox label="yes" defaultChecked />
+    );
+
+    expect(rendered.containsMatchingElement(<input type="checkbox" defaultChecked />)).toEqual(true);
   });
 });
