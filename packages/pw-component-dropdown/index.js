964ca2042a3b9f56d9bd377e57912a1b6bc0e135
Quinn Strahl <qstrahl@gmail.com>
2017-02-08 12:56:36 -0500

    Make Dropdown pass (value, event) to onChange handler

--- packages/pw-component-dropdown/index.js
+++ packages/pw-component-dropdown/index.js
@@ -1,31 +1,44 @@
 import React, { PropTypes } from 'react';
 import cssModules from 'react-css-modules';
 import styles from './style.css';
+import { omit } from 'lodash';
 
 const createOption = option => (
-  <option key={`option-${option.label}-${option.value}`} value={option.label}>{option.label}</option>
+  <option key={`option-${option.label}-${option.value}`} value={option.value}>{option.label || option.value}</option>
 );
 
-function Dropdown({ values, label }) {
-  return (
-    <select styleName="base" name={label}>
-      {values && values.map(e => createOption(e))}
-    </select>
-  );
+class Dropdown extends React.Component {
+  static propTypes = {
+    invalid: PropTypes.bool,
+    label: PropTypes.string,
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    tabIndex: PropTypes.number,
+    values: PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string,
+        value: PropTypes.oneOfType([
+          PropTypes.string,
+          PropTypes.bool,
+          PropTypes.number,
+        ]),
+      })
+    ),
+  };
+
+  handleChange = event => this.props.onChange(event.target.value, event);
+
+  render() {
+    const { values, label, ...props } = this.props;
+    const passthroughProps = omit(props, ['styles']);
+    return (
+      <select styleName="base" name={label} {...passthroughProps} onChange={this.handleChange}>
+        {values && values.map(e => createOption(e))}
+      </select>
+    );
+  }
 }
 
-Dropdown.propTypes = {
-  values: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      value: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.bool,
-        PropTypes.number,
-      ]),
-    })
-  ),
-  label: PropTypes.string,
-};
 
 export default cssModules(Dropdown, styles);
